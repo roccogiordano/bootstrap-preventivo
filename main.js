998880578995bfaad5f813224e2d347920727411
roccogiordano <rocco.giordano573@gmail.com>
2024-12-08 20:51:37 +0100

    feat: added more variables

--- main.js
+++ main.js
@@ -8,12 +8,32 @@ const backendPrice = 20.50;
 
 const analysisPrice = 33.60;
 
-const inputTypeSection = document.getElementById("inputType");
+const getQuoteForm = document.getElementById("getQuoteForm");
 
-const inputDiscountSection = document.getElementById("inputDiscount");
+const inputFName = document.getElementById("inputFName");
+
+const inputLName = document.getElementById("inputLName");
+
+const inputEmail = document.getElementById("inputEmail");
+
+const inputType = document.getElementById("inputType");
+
+const inputNotes = document.getElementById("inputNotes");
+
+const inputDiscount = document.getElementById("inputDiscount");
+
+const inputPrivacy = document.getElementById("inputPrivacy");
+
+const priceDisplay1 = document.getElementById("priceDisplay1");
+
+const priceDisplay2 = document.getElementById("priceDisplay2");
 
 let inputTypeValue;
 
 let inputDiscountValue;
 
+let price;
+
 //
+
+console.log(hoursNum, frontendPrice, backendPrice, analysisPrice, getQuoteForm, inputFName, inputLName, inputEmail, inputType, inputNotes, inputPrivacy, priceDisplay1, priceDisplay2, inputTypeValue, inputDiscountValue, price);
