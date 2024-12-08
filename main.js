b326c184cca639d3229d074eb84dc07994e7efa6
roccogiordano <rocco.giordano573@gmail.com>
2024-12-08 19:20:51 +0100

    feat: added variables initialization

---
+++ main.js
@@ -0,0 +1,19 @@
+// Variables Initialization
+
+const hoursNum = 10;
+
+const frontendPrice = 15.30;
+
+const backendPrice = 20.50;
+
+const analysisPrice = 33.60;
+
+const inputTypeSection = document.getElementById("inputType");
+
+const inputDiscountSection = document.getElementById("inputDiscount");
+
+let inputTypeValue;
+
+let inputDiscountValue;
+
+//
